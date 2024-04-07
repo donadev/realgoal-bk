63a66728161bedab207eaed858ad869a90938e22
Luigi Donadel <luigi.donadel@gmail.com>
2024-04-07 20:47:36 +0200

    Update PageLoader.js

--- api/services/PageLoader.js
+++ api/services/PageLoader.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer");
 const obtainPage = async function(url) {
     const browser = await puppeteer.launch({
         executablePath: '/usr/bin/google-chrome',
-        args: ['--no-sandbox']
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage()
     let content = null
