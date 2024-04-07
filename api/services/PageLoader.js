6e7a453513837768097b78ba76214fd94802652f
Luigi Donadel <luigi.donadel@gmail.com>
2024-04-07 22:32:41 +0200

    this can fix the browser problem

--- api/services/PageLoader.js
+++ api/services/PageLoader.js
@@ -1,10 +1,22 @@
 const puppeteer = require("puppeteer");
 
 const obtainPage = async function(url) {
-    const browser = await puppeteer.launch({
-        //executablePath: '/usr/bin/google-chrome',
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
+    let browser
+    if (process.env.NODE_ENV !== 'development') {
+        const chromium = require('@sparticuz/chromium')
+        // Optional: If you'd like to disable webgl, true is the default.
+        chromium.setGraphicsMode = false
+        const puppeteer = require('puppeteer-core')
+        browser = await puppeteer.launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath(),
+            headless: chromium.headless,
+        })
+    } else {
+        const puppeteer = require('puppeteer')
+        browser = await puppeteer.launch({headless: 'new'})
+    }
     const page = await browser.newPage()
     let content = null
     try {
