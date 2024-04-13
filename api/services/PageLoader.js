0b7eb902cb593fbde511156c4742d5dcaff9eda0
Luigi Donadel <luigi.donadel@gmail.com>
2024-04-13 13:32:04 +0200

    fix

--- api/services/PageLoader.js
+++ api/services/PageLoader.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer-core')
 const os = require("os");
 
 const obtainPage = async function(url, selectorToWaitFor) {
-    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox'], executablePath: `/opt/render/project/src/.cache/puppeteer/chrome-headless-shell/linux-123.0.6312.105/chrome-headless-shell-linux64/chrome-headless-shell`});
+    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
     const page = await browser.newPage()
     let content = null
 
